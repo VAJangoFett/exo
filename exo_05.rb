b369484e25b75611fee942b440f21510cd5a4c29
Jango <arthur.vouloir@gmail.com>
2018-09-28 10:18:35 +0200

    exo_05.rb les #{} incorpore des calculs dans zone texte

---
+++ exo_05.rb
@@ -0,0 +1,36 @@
+#Affiche le texte
+puts "On va compter le nombre d'heures de travail à THP"
+#Affiche le texte et calcule
+puts "Travail : #{10 * 5 * 11}"
+#Affiche le texte et calcule
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+#Affiche le texte
+puts "Et en secondes ?"
+
+#Calcul
+puts 10 * 5 * 11 * 60 * 60
+
+#Affiche le texte
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#Calcul
+puts 3 + 2 < 5 - 7
+
+#Affiche le texte et calcule
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#Affiche le texte et calcule
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+#Affiche le texte
+puts "Ok, c'est faux alors !"
+
+#Affiche le texte
+puts "C'est drôle ça, faisons-en plus :"
+
+#Affiche le texte et calcule
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#Affiche le texte et calcule
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#Affiche le texte et calcule
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
